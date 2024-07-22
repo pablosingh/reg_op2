36452f26323d1d24faba3161fed1bacc7d5728fe
pablosingh <pablo22_4@hotmail.com>
2024-07-22 20:40:18 -0300

    configurando middleware thunk

--- src/components/Holding.jsx
+++ src/components/Holding.jsx
@@ -1,16 +1,19 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import CardTicker from "./CardTicker";
 import CardHolding from "./CardHolding";
+import { actualPrice } from '../redux/holdings/actions';
 
 export default function Holding() {
     const ops = useSelector( state => state?.operations.operations );
     const hol = useSelector( state => state?.holdings.holdings );
+    const dispatch = useDispatch();
     return (
         <Container>
             {ops && ops.map( t => <CardTicker ticker={t} /> )}
             ---------------------------------------------------------
             {hol && hol.map( t => <CardHolding ticker={t} /> )}
+            {/* <button onClick={()=> dispatch(actualPrice()) }>dispatch</button> */}
         </Container>
     );
 };
--- src/redux/holdings/actions.js
+++ src/redux/holdings/actions.js
@@ -7,3 +7,24 @@ export const average = (op) => {
     };
 };
 
+export function actualPrice () {
+    return async function (dispatch) {
+        const apiURL = `https://www.binance.us/api/v3/ticker/price?symbol=btcusdt`;
+        try{
+            await fetch(apiURL, {
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                  // Otros encabezados necesarios según la API (por ejemplo, tokens de autorización)
+                },
+                mode: 'cors', // Modo CORS
+              })
+                .then( js => js.json() )
+                .then( data => console.log(data) )
+                .catch( err => console.error(err) )
+        }catch(e){
+            console.error(e);
+        };
+    };  
+};
+
