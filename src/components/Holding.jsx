bca8cdf27f85deefb10cc58076b482529cd71b22
pablosingh <pablo22_4@hotmail.com>
2024-08-18 13:49:55 -0300

    boton de estado

--- src/components/Holding.jsx
+++ src/components/Holding.jsx
@@ -6,6 +6,7 @@ export default function Holding() {
     const hol = useSelector( state => state?.holdings.holdings );
     return (
         <Container>
+            <Btn onClick={()=> console.log(state)}>Estado</Btn>
             {hol && hol.map( t => <CardHolding ticker={t} key={t.id}/> )}
         </Container>
     );
@@ -18,4 +19,17 @@ const Container = styled.div`
     min-width: 50vw;
     padding: 0.9em 0em;
     margin: 0em;
+`;
+
+const Btn = styled.button`
+    background-color: rgba(8,108,9,1);
+    color: white;
+    border-radius: 1em;
+    padding: 0.2em 1em;
+    margin: 1em;
+    transition: all .4s ease;
+    &:hover{
+        background-color: rgba(8,108,9,0.5);
+        color: black;
+    }
 `;
