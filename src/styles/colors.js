ee2906f8b185972d560cb7413da2dc8ee9a63321
pablosingh <pablo22_4@hotmail.com>
2024-09-02 21:21:56 -0300

    styles

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,4 +1,4 @@
-export const primaryColor = 'rgba(43,153,190,255)';
-export const secondaryColor = 'rgb(204,231,232)';
-export const tertiaryColor = 'rgba(8,108,9,1)';
+export const primaryColor = 'rgba(111,64,22,255)';
+export const secondaryColor = 'rgba(210,180,142,255)';
+export const tertiaryColor = 'rgba(3,94,5,255)';
 export const tertiaryHoverColor = 'rgba(24,245,12,0.5)';
